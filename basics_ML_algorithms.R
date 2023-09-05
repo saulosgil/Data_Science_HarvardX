b56a89f946775d72e6137240784ba07ebcc48c90
saulosgil <saulosgil@hotmail.com>
2023-09-05 13:11:09 -0300

    performance metrics

--- basics_ML_algorithms.R
+++ basics_ML_algorithms.R
@@ -79,4 +79,57 @@ test_set |>
 
 prev <- mean(y == "Male")
 
-confusionMatrix(data = y_hat, reference = test_set$sex)
+caret::confusionMatrix(data = y_hat, reference = test_set$sex)
+
+# Sensitivity and Specificity -----------------------------------------------------------------
+# get the metrics from Confusion matrix
+
+cm <- caret::confusionMatrix(data = y_hat, reference = test_set$sex)
+
+# access specific metrics
+cm$overall["Accuracy"]
+
+cm$byClass[c("Sensitivity","Specificity", "Prevalence")]
+
+# Optimization --------------------------------------------------------------------------------
+
+# For optimization purposes, sometimes it is more useful to have a one number summary than
+# studying both specificity and sensitivity. One preferred metric is balanced accuracy.
+# Because specificity and sensitivity are rates, it is more appropriate to compute the harmonic
+# average. In fact, the F1-score, a widely used one-number summary, is the harmonic average of
+# precision and recall.
+
+# Depending on the context, some type of errors are more costly than others. The F1-score can be
+# adapted to weigh specificity and sensitivity differently.
+
+# You can compute the F1-score using the F_meas() function in the caret package.
+
+# maximize F-score
+cutoff <- seq(61, 70)
+F_1 <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") |>
+    factor(levels = levels(test_set$sex))
+  F_meas(data = y_hat, reference = factor(train_set$sex))
+})
+
+data.frame(cutoff, F_1) |>
+  ggplot(aes(cutoff, F_1)) +
+  geom_point() +
+  geom_line()
+
+max(F_1)
+
+best_cutoff_2 <- cutoff[which.max(F_1)]
+best_cutoff_2
+
+y_hat <- ifelse(test_set$height > best_cutoff_2, "Male", "Female") %>%
+  factor(levels = levels(test_set$sex))
+
+mean(y_hat == test_set$sex)
+
+sensitivity(data = y_hat, reference = test_set$sex)
+
+specificity(data = y_hat, reference = test_set$sex)
+
+
+
